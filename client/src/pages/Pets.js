e353c7c0483b06d46ba7fbd8f63b8500d5c70ba4
Tony Trinh <sojlit@gmail.com>
2020-01-13 02:29:57 +1100

    apollo react: querying mutations solution

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -16,19 +16,34 @@ const ALL_PETS = gql`
   }
 `;
 
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
 export default function Pets () {
   const [modal, setModal] = useState(false);
   const { data, loading, error } = useQuery(ALL_PETS);
+  const [ createPet, newPet ] = useMutation(NEW_PET);
 
   const onSubmit = input => {
     setModal(false);
+    createPet({
+      variables: { newPet: input }
+    });
   }
 
-  if (loading) {
+  if (loading || newPet.loading) {
     return <Loader />;
   }
 
-  if (error) {
+  if (error || newPet.error) {
     <p>error!</p>;
   }
   
